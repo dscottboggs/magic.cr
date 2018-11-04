f4c75abd221ef79c76421e36c1afa0144c2d2524
D. Scott Boggs <scott@tams.tech>
2018-11-04 01:27:10 -0400

    Added documentation for the module-level functions.

--- src/magic.cr
+++ src/magic.cr
@@ -10,16 +10,23 @@ module Magic
 
   extend self
 
+  # Get the filetype of the given File object, filepath, file descriptor, or
+  # byte stream (IO) as a plain, human-readable string.
   def filetype_of(this)
     check_filetype = @@filetype_checker ||= Magic::TypeChecker.new
     check_filetype.of this
   end
 
+  # Get the mime-type of the given File object, filepath, file descriptor, or
+  # byte stream (IO). This is the easiest to use if you're comparing the
+  # filetypes of several files.
   def mime_type_of(this)
     check_mime_type = @@mime_type_checker ||= Magic::TypeChecker.new.get_mime_type
     check_mime_type.of this
   end
 
+  # Get the set of valid extensions for the given File object, filepath,
+  # file descriptor, or byte stream (IO).
   def valid_extensions_for(this) : Set(String)
     get_valid_extensions = @@extension_getter ||= Magic::TypeChecker.new.extensions
     get_valid_extensions.of(this).split("/").to_set
